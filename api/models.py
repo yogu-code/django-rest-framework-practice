b185d4cef0f77470db844ec0c922d04a79e96524
yogu-code <yogeshrane019@gmail.com>
2026-06-24 13:42:42 +0530

    Initial project setup with Django, including models, migrations, and basic configuration files.

---
+++ api/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
+
+
+class User(AbstractUser):
+    pass
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    image = models.ImageField(upload_to="product/", blank=True, null=True)
+
+    @property
+    def in_stock(self):
+        return self.stock > 0
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    class StatusChoices(models.TextChoices):
+        PENDING = "pending"
+        CONFIRMED = "confirmed"
+        CANCELLED = "canclled"
+
+    order_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=20, choices=StatusChoices.choices, default=StatusChoices.PENDING
+    )
+    product = models.ManyToManyField(
+        Product, through="OrderItem", related_name="orders"
+    )
+
+    def __str__(self):
+        return f"order {self.order_id} by {self.user} "
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    @property
+    def total_price(self):
+        return self.quantity * self.product.price
+
+    def __str__(self):
+        return f"{self.quantity} X {self.product.name} by {self.order.order_id}"
